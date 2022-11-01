84493733ee4d3c629772e729e72d5777ebebb486
Jimmy Sanchez <jimmyej23@gmail.com>
2022-11-01 13:55:55 -0500

    adding logs

--- src/main/java/com/store/storeapp/controllers/ProductController.java
+++ src/main/java/com/store/storeapp/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.store.storeapp.controllers;
 
 import com.store.storeapp.entities.Product;
 import com.store.storeapp.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
@@ -22,10 +24,13 @@ public class ProductController {
         try{
             List<Product> products = service.getProducts();
             if (products.isEmpty()) {
+                log.warn("Data not found");
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
+            log.info("Data found");
             return new ResponseEntity<>(products, HttpStatus.OK);
         } catch (Exception e) {
+            log.error("Server error");
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -34,8 +39,10 @@ public class ProductController {
     ResponseEntity<Product> getProductById(@PathVariable Integer id){
         Optional<Product> product = service.getProductById(id);
         if (product.isPresent()) {
+            log.info("Data found");
             return new ResponseEntity<>(product.get(), HttpStatus.OK);
         } else {
+            log.warn("Data not found");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -44,8 +51,10 @@ public class ProductController {
     ResponseEntity<Product> createProduct(@RequestBody Product product){
         try {
             Product newProduct = service.createProduct(product);
+            log.info("Product created");
             return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
         } catch (Exception e) {
+            log.error("Server error");
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -54,8 +63,10 @@ public class ProductController {
     ResponseEntity<Product> updateProduct(@RequestBody Product product){
         try {
             Product newProduct = service.updateProduct(product);
+            log.info("Product updated");
             return new ResponseEntity<>(newProduct, HttpStatus.OK);
         } catch (Exception e) {
+            log.error("Server error");
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -64,8 +75,10 @@ public class ProductController {
     ResponseEntity<HttpStatus> deleteProduct(@PathVariable Integer id){
         try {
             service.deleteProductById(id);
+            log.info("Product deleted");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
+            log.error("Server error");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -74,12 +87,14 @@ public class ProductController {
     ResponseEntity<List<Product>> getActiveProducts(@PathVariable boolean isActive){
         try {
             List<Product> activeProducts = service.getActiveProducts(isActive);
-
             if (activeProducts.isEmpty()) {
+                log.warn("Data not found");
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
+            log.info("Data found");
             return new ResponseEntity<>(activeProducts, HttpStatus.OK);
         } catch (Exception e) {
+            log.error("Server error");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
